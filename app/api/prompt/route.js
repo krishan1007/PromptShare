ffae5b043656d1dc0e792bda2c173fe45aaeee19
krishan kumar <rambadgyo@gmail.com>
2023-06-20 01:18:59 +0530

    worked around issue of feed reloading prompts

--- app/api/prompt/route.js
+++ app/api/prompt/route.js
@@ -5,9 +5,25 @@ import User from "@models/user";
 export const GET  = async (request) =>{
     try {
         await connectToDB();
-        const prompts = await Prompt.find({}).populate('creator');
+        const prompt = await Prompt.find().populate({
+          path: "creator"
+        });
 
-        return new Response(JSON.stringify(prompts),{status:200});
+        const response = new Response(JSON.stringify(prompt), {
+          status: 200,
+        });
+
+        
+        // Add a unique identifier to the URL to force a cache-busting reload
+        const url = new URL(request.url);
+        url.searchParams.set("t", Date.now());
+        response.headers.set("Cache-Control", "no-cache, no-store, must-revalidate");
+        response.headers.set("Pragma", "no-cache");
+        response.headers.set("Expires", "0");
+        response.headers.set("Location", url.toString());
+
+        return response;
+
 
     } catch (error) {
          return new Response("Failed to fetch all prompts",{status:500});
